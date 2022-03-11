450475fdd2079d8587a1bc5fbafce4d2e7a00af7
Jorge Sanchez <jorge.asc@outlook.com>
2022-03-10 17:59:25 -0700

    arreglando mi mequez

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,32 +5,30 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
+
 use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+    /* 
      * Register any application services.
      *
      * @return void
      */
     public function register()
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
-    /**
+    /*
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot(UrlGenerator $url)
     {
-        if (env('REDIRECT_HTTPS')) {
-
-            $this->app['request']->server->set('HTTPS', true);
-
-        }
         Inertia::share('flash', function() {
             return ['status' => Session::get('status')];
         });
